4cb3cfbea1703dba9028c3fd982ca7dbe50fb5b1
tienv1per <tienv1perds@gmail.com>
2026-05-25 22:45:07 +0700

    add broker

--- broker.go
+++ broker.go
@@ -6,57 +6,107 @@ import (
 	"net"
 )
 
-func readFromStream(stream_rw *bufio.ReadWriter) (*string, error) {
+const BROKER_PORT = 10000
+const (
+	ECHO = 1
+	// other message types
+)
+
+type Message struct {
+	// Nếu ECHO != nil thì message này là echo request
+	ECHO *string
+}
+
+func readFromStream(stream_rw *bufio.ReadWriter) ([]byte, error) {
 	var err error
+	// Protocol: [length][data]
 	header, err := stream_rw.ReadByte()
 	if err != nil {
 		return nil, err
 	}
+	// Đọc đúng số byte mà header báo
 	data, err := stream_rw.Peek(int(header))
 	if err != nil {
 		return nil, err
 	}
+	// Bỏ data đã đọc ra khỏi buffer
 	_, err = stream_rw.Discard(int(header))
-	var data_str = string(data)
-	return &data_str, err
+	return data, err
 }
+
 func writeToStream(stream_rw *bufio.ReadWriter, data string) error {
 	var err error
-	// write
+	// Gửi response theo protocol: [length][data]
 	err = stream_rw.WriteByte(byte(len(data)))
 	if err != nil {
 		return err
 	}
+	// Ghi nội dung response
 	_, err = stream_rw.WriteString(data)
 	if err != nil {
 		return err
 	}
+	// Đẩy buffer ra TCP connection
 	stream_rw.Flush()
 	return nil
 }
-
-const BROKER_PORT = 10000
 
 type Broker struct {
 }
 
 func (b *Broker) startBrokerServer() error {
+	// Mở TCP server tại BROKER_PORT
 	listener, _ := net.Listen("tcp", fmt.Sprintf(":%d", BROKER_PORT))
 	for {
+		// Chờ client kết nối
 		conn, _ := listener.Accept()
 		streamRW := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+		// Đọc raw message từ TCP stream
 		data, err := readFromStream(streamRW)
 		if err != nil {
 			return err
 		}
-		// write it back
-		err = writeToStream(streamRW, *data)
-		if err != nil {
-			return err
+		// Parse raw message thành Message theo type
+		parsed_message := b.parseBrokerMessage(data)
+		if parsed_message != nil {
+			// Xử lý message và tạo response
+			resp, err := b.processBrokerMessage(parsed_message)
+			if err != nil {
+				fmt.Print("error1", err)
+				return err
+			}
+			// Gửi response về client
+			err = writeToStream(streamRW, resp)
+			if err != nil {
+				fmt.Print("error2", err)
+				return err
+			}
 		}
 		err = conn.Close()
 		if err != nil {
 			return err
 		}
 	}
+}
+
+func (b *Broker) parseBrokerMessage(message []byte) *Message {
+	// Byte đầu tiên là message type
+	switch message[0] {
+	case ECHO:
+		// Các byte còn lại là nội dung echo
+		var st = string(message[1:])
+		return &Message{ECHO: &st}
+	default:
+		return nil
+	}
+}
+
+func (b *Broker) processBrokerMessage(message *Message) (string, error) {
+	var err error
+	var resp string
+	if message.ECHO != nil {
+		// Echo response cho client
+		resp = fmt.Sprintf("I have receiver: %s", *message.ECHO)
+	}
+	return resp, err
 }
--- main.go
+++ main.go
@@ -3,84 +3,77 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
-	"sync"
-	"time"
 )
 
 func main() {
 	if os.Args[1] == "server" {
-		spawnServer()
-	} else {
-		clientConnect(os.Args[2])
-	}
-}
-
-func spawnServer() {
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		startServer("10001")
-	}()
-	go func() {
-		defer wg.Done()
-		startServer("10002")
-	}()
-	wg.Wait()
-}
-
-func startServer(port string) {
-	ln, _ := net.Listen("tcp", fmt.Sprintf(":%s", port))
-	conn, _ := ln.Accept() // Chờ client kết nối
-	stream_rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-	for {
-		// Protocol: [1 byte độ dài][n byte dữ liệu]
-		header, _ := stream_rw.ReadByte()      // Đọc độ dài message, block nếu chưa có dữ liệu
-		data, _ := stream_rw.Peek(int(header)) // Nhìn n byte tiếp theo, chưa lấy khỏi buffer
-		fmt.Printf("Data from client: %s\n", data)
-		if strings.Trim(string(data), "\n ") == "bye" {
-			break
-		}
-		stream_rw.Discard(int(header))      // Bỏ data đã đọc ra khỏi buffer
-		time.Sleep(2000 * time.Millisecond) // Giả lập xử lý chậm
-
-		newData := fmt.Sprintf("Received from client: %s", string(data))
-		stream_rw.WriteByte(byte(len(newData))) // Header đang dùng độ dài data gốc
-		stream_rw.WriteString(newData)       // Ghi response vào buffer
-		stream_rw.Flush()                    // Đẩy buffer ra TCP connection
-	}
-	conn.Close()
-}
-
-func clientConnect(port string) {
-	conn, _ := net.Dial("tcp", fmt.Sprintf(":%s", port))
-	for {
-		// Đọc 1 dòng người dùng nhập từ terminal
-		rd := bufio.NewReader(os.Stdin)
-		line, err := rd.ReadString('\n')
+		// Chạy broker TCP server
+		var broker = Broker{}
+		err := broker.startBrokerServer()
 		if err != nil {
-			return
+			fmt.Printf("Error starting broker: %v\n", err.Error())
 		}
-		fmt.Printf("Send to server: %s\n", line)
-
-		stream_rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-		// Gửi theo protocol: [1 byte độ dài][n byte dữ liệu]
-		stream_rw.WriteByte(byte(len(line))) // Gửi độ dài message
-		stream_rw.WriteString(line)          // Gửi nội dung message
-		stream_rw.Flush()                    // Đẩy buffer ra TCP connection
-		if strings.Trim(line, "\n ") == "bye" {
-			break
-		}
-
-		// Đọc response từ server theo cùng protocol
-		header, _ := stream_rw.ReadByte()      // Đọc độ dài response
-		data, _ := stream_rw.Peek(int(header)) // Nhìn n byte tiếp theo, chưa lấy khỏi buffer
-		fmt.Printf("Data from server: %s\n", data)
-		stream_rw.Discard(int(header)) // Bỏ response đã đọc ra khỏi buffer
+	} else {
+		// Chạy client và gửi message ECHO tới broker
+		clientConnectTCPAndEcho(BROKER_PORT)
 	}
+}
 
-	conn.Close()
+func writeEchoToStream(streamRW *bufio.ReadWriter, data string) error {
+	var err error
+	// Protocol request: [length][type][data]
+	err = streamRW.WriteByte(byte(len(data) + 1))
+	if err != nil {
+		return err
+	}
+	// Type ECHO cho broker biết đây là message echo
+	err = streamRW.WriteByte(ECHO)
+	if err != nil {
+		return err
+	}
+	// Ghi nội dung message sau type
+	_, err = streamRW.WriteString(data)
+	if err != nil {
+		return err
+	}
+	// Đẩy toàn bộ buffer ra TCP connection
+	err = streamRW.Flush()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func clientConnectTCPAndEcho(port int) {
+	conn, _ := net.Dial("tcp", fmt.Sprintf(":%d", port))
+	fmt.Printf("Connected to server at port %v\n", port)
+
+	// Đọc 1 dòng từ terminal
+	reader := bufio.NewReader(os.Stdin)
+	streamRW := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		if err == io.EOF {
+			return
+		} else {
+			// panic here
+		}
+	}
+	fmt.Printf("Sent to server: %s\n", line)
+	writeEchoToStream(streamRW, strings.Trim(line, "\n"))
+
+	// Đọc response từ broker theo protocol: [length][data]
+	header, err := streamRW.ReadByte()
+	if header == 0 || err != nil {
+		return
+	}
+	// Peek chỉ nhìn data, chưa bỏ khỏi buffer
+	data, _ := streamRW.Peek(int(header))
+	fmt.Printf("Receive message from server: %s\n", data)
+	// Bỏ response đã đọc ra khỏi buffer
+	streamRW.Discard(int(header))
 }
